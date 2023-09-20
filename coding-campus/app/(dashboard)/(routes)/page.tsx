375bc5e36e6ee7ac615f04dcdf3c60d8f7fdd563
Kirill Salfetnik <oriole001@gmail.com>
2023-09-20 11:20:28 +0200

    Finish authentication using Clerk

--- coding-campus/app/(dashboard)/(routes)/page.tsx
+++ coding-campus/app/(dashboard)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { UserButton } from "@clerk/nextjs"
+
 export default function Home() {
   return (
     <div>
-      <p className='text-3xl font-medium text-sky-700'>
-        This is a protected page
-      </p>
+      <UserButton 
+        afterSignOutUrl="/"
+      />
     </div>
   )
 }
