d8e7808413fe3b4886e6336d21cdf82bf98df324
Kirill Salfetnik <oriole001@gmail.com>
2023-11-11 17:46:48 +0100

    Implement the UI for drag-and-drop chapters

--- coding-campus/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ coding-campus/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -21,6 +21,8 @@ import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { Input } from "@/components/ui/input";
 
+import { ChaptersList } from "./chapters-list";
+
 interface ChaptersFormProps {
     initialData: Course & { chapters: Chapter[] };
     courseId: string;
@@ -115,7 +117,11 @@ export const ChaptersForm = ({
                     !initialData.chapters.length && "text-slate-500 italic"
                 )}>
                     {!initialData.chapters.length && "No chapters"}
-                    {/* TODO: Add a list of chapters */}
+                    <ChaptersList 
+                        onEdit={() => {}}
+                        onReorder={() => {}}
+                        items={initialData.chapters || []}
+                    />
                 </div>
             )}
             {!isCreating && (
